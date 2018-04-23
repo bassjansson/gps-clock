472bdc8bfeda847a11dd2cfe5f35644d8e05e7b4
Bass Jansson <bassjansson@gmail.com>
2018-04-23 11:47:08 +0200

    Adds RTC to main arduino file.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,28 +1,18 @@
-// Configure NeoGPS
+//=============================//
+//========== Defines ==========//
+//=============================//
+
+// Enable GPS interrupt processing
 //#define NMEAGPS_INTERRUPT_PROCESSING
 
-// Configure NeoSWSerial
+// GPS serial port pins
 #define GPS_PORT_RX_PIN 2 // Digital Pin 2 (attached to TX of GPS module)
 #define GPS_PORT_TX_PIN 3 // Digital Pin 3 (attached to RX of GPS module)
 
-// Include NeoGPS & NeoSWSerial
-#include <NMEAGPS.h>
-#include <NeoSWSerial.h>
+// Local time zone offset
+#define TIME_ZONE (+1) // UTC+1 (Amsterdam)
 
-// Check NeoGPS configuration
-#if !defined(GPS_FIX_TIME) | !defined(GPS_FIX_DATE)
-#error You must define GPS_FIX_TIME and GPS_FIX_DATE in GPSfix_cfg.h!
-#endif
-
-#if !defined(NMEAGPS_PARSE_RMC) & !defined(NMEAGPS_PARSE_ZDA)
-#error You must define NMEAGPS_PARSE_RMC or NMEAGPS_PARSE_ZDA in NMEAGPS_cfg.h!
-#endif
-
-// Define local time zone offset
-static const int32_t timeZoneHours = (+1); // UTC+1 (Amsterdam)
-static const NeoGPS::clock_t timeZoneOffset = timeZoneHours * NeoGPS::SECONDS_PER_HOUR;
-
-// Define EU Daylight Saving Time
+// EU daylight saving time configuration
 static const struct
 {
     static const uint8_t springMonth =  3; // March
@@ -34,12 +24,48 @@ static const struct
     static const uint8_t fallHour    =  2; // 2AM
 } EU_DST;
 
-// Declare GPS parser and fix
+
+//==============================//
+//========== Includes ==========//
+//==============================//
+
+#include <Wire.h>
+#include <RTClib.h>
+#include <NMEAGPS.h>
+#include <NeoSWSerial.h>
+
+
+//====================================//
+//========== NeoGPS Config ===========//
+//====================================//
+
+#if !defined(GPS_FIX_TIME) | !defined(GPS_FIX_DATE)
+#error You must define GPS_FIX_TIME and GPS_FIX_DATE in GPSfix_cfg.h!
+#endif
+
+#if !defined(NMEAGPS_PARSE_RMC) & !defined(NMEAGPS_PARSE_ZDA)
+#error You must define NMEAGPS_PARSE_RMC or NMEAGPS_PARSE_ZDA in NMEAGPS_cfg.h!
+#endif
+
+
+//==============================//
+//========== Objects ===========//
+//==============================//
+
+// GPS parser and fix
 static NMEAGPS gpsParser; // This parses received characters
 static gps_fix gpsFix;    // This contains all the parsed pieces
 
-// Declare GPS serial port
+// GPS serial port
 static NeoSWSerial gpsPort(GPS_PORT_RX_PIN, GPS_PORT_TX_PIN);
+
+// RTC object
+RTC_DS1307 rtc;
+
+
+//==============================//
+//========== Methods ===========//
+//==============================//
 
 // Adjusts time by local time zone and DST
 void adjustTime(NeoGPS::time_t &dt)
@@ -80,7 +106,7 @@ void adjustTime(NeoGPS::time_t &dt)
     }
 
     // First offset from UTC to the local time zone
-    seconds += timeZoneOffset;
+    seconds += TIME_ZONE * NeoGPS::SECONDS_PER_HOUR;
 
     // Then add an hour if DST is in effect
     if ((springForward <= seconds) && (seconds < fallBack))
@@ -90,7 +116,8 @@ void adjustTime(NeoGPS::time_t &dt)
     dt = seconds;
 }
 
-// Read GPS time from the GPS serial port and print it
+// Reads GPS time from the GPS serial port and prints it
+// Also sets the RTC time to the GPS time
 static void readAndPrintGPSTime()
 {
     while (gpsParser.available(gpsPort))
@@ -105,7 +132,10 @@ static void readAndPrintGPSTime()
             Serial << gpsFix.dateTime;
             Serial.println();
 
-            // For example to get hours: gpsFix.dateTime.hours
+            // Set RTC time to GPS time
+            rtc.adjust(DateTime((NeoGPS::clock_t)gpsFix.dateTime + SECONDS_FROM_1970_TO_2000));
+
+            break;
         }
     }
 }
@@ -132,11 +162,36 @@ void setup()
     gpsPort.attachInterrupt(gpsParserHandler);
     Serial.println("GPS interrupt processing enabled!");
 #endif
+
+    // Begin RTC
+    rtc.begin();
 }
 
 // Arduino loop
+DateTime rtcTime;
 void loop()
 {
     // Read and print GPS time
     readAndPrintGPSTime();
+
+    // Read and print RTC time
+    DateTime now = rtc.now();
+    if (now.secondstime() > rtcTime.secondstime())
+    {
+        rtcTime = now;
+
+        Serial.print("RTC Time: ");
+        Serial.print(now.year(), DEC);
+        Serial.print('-');
+        Serial.print(now.month(), DEC);
+        Serial.print('-');
+        Serial.print(now.day(), DEC);
+        Serial.print(' ');
+        Serial.print(now.hour(), DEC);
+        Serial.print(':');
+        Serial.print(now.minute(), DEC);
+        Serial.print(':');
+        Serial.print(now.second(), DEC);
+        Serial.println();
+    }
 }
