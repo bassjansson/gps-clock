74e37e2ffb7e653bed77d9f4820f278f47f3d5d6
Bass Jansson <tech@bassjansson.com>
2018-08-23 09:23:58 +0200

    Updates entire program with final settings and improvements.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,14 +6,15 @@
 //#define SERIAL_DEBUG
 
 // Motor settings
-#define MOTOR_DIRECTION   HIGH // HIGH or LOW
-#define MOTOR_START_SPEED 128  // 0 - 255
-#define MOTOR_MAX_SPEED   255  // 0 - 255
+#define MOTOR_DIRECTION    HIGH // HIGH or LOW (OFFICAL)
+#define MOTOR_START_SPEED  109  // 0 - 255 (OFFICAL)
+#define MOTOR_DOUBLE_SPEED 200  // 0 - 255 (OFFICAL)
 
 // Metal sensor settings
-#define METAL_SENSOR_INVERT_INPUT false // true or false
-#define METAL_SENSOR_HOUR_PIN     11    // Digital Pin 11
-#define METAL_SENSOR_MINUTE_PIN   12    // Digital Pin 12
+#define METAL_SENSOR_INVERT_INPUT true // true or false  (OFFICAL)
+#define METAL_SENSOR_HOUR_PIN     12   // Digital Pin 12 (OFFICAL)
+#define METAL_SENSOR_MINUTE_PIN   11   // Digital Pin 11 (OFFICAL)
+#define METAL_SENSOR_HOUR_POS     3    // Hours (0 - 11) (OFFICAL)
 
 // GPS serial port pins
 #define GPS_PORT_RX_PIN 2 // Digital Pin 2 (attached to TX of GPS module)
@@ -75,6 +76,9 @@ static NeoSWSerial gpsPort(GPS_PORT_RX_PIN, GPS_PORT_TX_PIN);
 
 // RTC object
 static RTC_DS1307 rtc;
+
+// Clock time
+static int32_t clockTime = 0; // seconds
 
 
 //======================================//
@@ -241,7 +245,7 @@ bool isClockAtZeroMinutes()
 //========== Clock Methods ===========//
 //====================================//
 
-NeoGPS::clock_t getTargetClockTime()
+int32_t getTargetClockTime()
 {
     // Adjust RTC time to GPS time
     adjustRTCTimeToGPSTime();
@@ -258,38 +262,79 @@ NeoGPS::clock_t getTargetClockTime()
 void setClockToTargetClockTime()
 {
     // Set clock time to zero
-    NeoGPS::clock_t clockTime = 0;
+    int32_t clockTime = METAL_SENSOR_HOUR_POS * NeoGPS::SECONDS_PER_HOUR; // The hour sensor is located at 03:00
 
-    // Set motor to maximum speed
-    setMotorSpeed(MOTOR_MAX_SPEED);
+    // Set motor to double speed
+    setMotorSpeed(MOTOR_DOUBLE_SPEED);
 
     // Wait until we hit zero minutes and zero hours
     while (!(isClockAtZeroMinutes() && isClockAtZeroHours())) void;
 
-    // Wait until we passed zero minutes
-    while (isClockAtZeroMinutes()) void;
+    #ifdef SERIAL_DEBUG
+        Serial.println("[setClockToTargetClockTime] Clock reached initial position of 03:00");
+    #endif
 
-    // Increment clock time until we reached target clock time
-    while (clockTime < getTargetClockTime())
+    while (true)
     {
-        // Wait until we hit zero minutes again
+        int32_t targetClockTimeFull = getTargetClockTime();
+        int32_t targetClockTimeMinutes = targetClockTimeFull % NeoGPS::SECONDS_PER_HOUR;
+        int32_t targetClockTimeHours = targetClockTimeFull - targetClockTimeMinutes;
+    
+        if (clockTime == targetClockTimeHours)
+        {
+            #ifdef SERIAL_DEBUG
+                Serial.print("[setClockToTargetClockTime] All hours added, remaining minutes to add: ");
+                Serial.println(targetClockTimeMinutes * 2 / NeoGPS::SECONDS_PER_MINUTE);
+            #endif
+            
+            int32_t clockTimeWhenFullySet = (targetClockTimeFull + targetClockTimeMinutes) % (NeoGPS::SECONDS_PER_HOUR * 12);
+
+            while (true)
+            {
+                // Get clock time difference between current and target clock time
+                int32_t clockTimeDifference = clockTimeWhenFullySet - getTargetClockTime();
+        
+                // Adjust clock time difference if it is bigger than six hours
+                if (abs(clockTimeDifference) > NeoGPS::SECONDS_PER_HOUR * 6)
+                    clockTimeDifference -= NeoGPS::SECONDS_PER_HOUR * 12 * (clockTimeDifference > 0 ? 1 : -1);
+                    
+                if (clockTimeDifference <= 0)
+                    break;
+            }
+
+            // Set motor to start speed
+            setMotorSpeed(MOTOR_START_SPEED);
+
+            #ifdef SERIAL_DEBUG
+                Serial.println("[setClockToTargetClockTime] Clock reached target clock time! Now running at normal speed");
+            #endif
+
+            // Delay to make sure we passed zero minutes
+            if (targetClockTimeMinutes % 1800 < 300)
+                delay(600 * 1000); // 10 minutes
+    
+            return;
+        }
+
+        // Delay to make sure we passed zero minutes
+        delay(300 * 1000); // 5 minutes
+
+        // Wait until we hit zero minutes
         while (!isClockAtZeroMinutes()) void;
 
         // Add one hour to clock time
         clockTime = (clockTime + NeoGPS::SECONDS_PER_HOUR) % (NeoGPS::SECONDS_PER_HOUR * 12);
 
-        // Wait until we passed zero minutes
-        while (isClockAtZeroMinutes()) void;
+        #ifdef SERIAL_DEBUG
+            Serial.print("[setClockToTargetClockTime] Added one hour to clock time: ");
+            Serial.println(clockTime / NeoGPS::SECONDS_PER_HOUR);
+        #endif
     }
-
-    // Set motor to start speed
-    setMotorSpeed(MOTOR_START_SPEED);
 }
 
 void adjustClockSpeed()
 {
     // Declare static clock time and clock speed
-    static NeoGPS::clock_t clockTime = 0; // seconds
     static byte clockSpeed = MOTOR_START_SPEED;
 
     // Check if it is time to adjust the clock speed
@@ -297,12 +342,12 @@ void adjustClockSpeed()
     {
         // Update clock time
         if (isClockAtZeroHours())
-            clockTime = 0;
+            clockTime = METAL_SENSOR_HOUR_POS * NeoGPS::SECONDS_PER_HOUR; // The hour sensor is located at 03:00
         else
             clockTime = (clockTime + NeoGPS::SECONDS_PER_HOUR) % (NeoGPS::SECONDS_PER_HOUR * 12);
 
         // Get clock time difference between current and target clock time
-        NeoGPS::clock_t clockTimeDifference = clockTime - getTargetClockTime();
+        int32_t clockTimeDifference = clockTime - getTargetClockTime();
 
         // Adjust clock time difference if it is bigger than six hours
         if (abs(clockTimeDifference) > NeoGPS::SECONDS_PER_HOUR * 6)
@@ -315,8 +360,8 @@ void adjustClockSpeed()
         // Set the motor speed to the clock speed
         setMotorSpeed(clockSpeed);
 
-        // Wait until we passed zero minutes
-        while (isClockAtZeroMinutes()) void;
+        // Delay until we passed zero minutes
+        delay(300 * 1000); // 5 minutes
     }
 }
 
