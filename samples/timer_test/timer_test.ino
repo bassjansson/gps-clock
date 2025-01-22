5663a1e7121c867a62689de05878f10e3d08fab2
Bass Jansson <tech@bassjansson.com>
2025-01-22 10:58:40 +0100

    Improves timer test with a better motor speed function.

--- samples/timer_test/timer_test.ino
+++ samples/timer_test/timer_test.ino
@@ -1,12 +1,14 @@
 #include <TimerOne.h>
 
-#define MOTOR_STEPS       200 // Full steps per revolution
-#define MOTOR_MICRO_STEPS 2   // Microsteps
-#define MOTOR_GEAR_RATIO  20  // Gear ratio
-
-#define MOTOR_RPM         8.5 // RPM after gear
-#define MOTOR_DIRECTION   LOW // HIGH = forward, LOW = backward
-#define MOTOR_PWM_DUTY    512 // 50% duty cycle
+#define MOTOR_STEPS       200    // Full steps per revolution
+#define MOTOR_MICRO_STEPS 2      // Microsteps (2 * 200 = 400)
+#define MOTOR_GEAR_RATIO  20     // Gear ratio
+#define MOTOR_RPM         8.5    // RPM after gear (20 * 8.5 = 170)
+#define MOTOR_DIRECTION   LOW    // HIGH = forward, LOW = backward
+#define MOTOR_PWM_DUTY    512    // 50% duty cycle, max 1024
+#define MOTOR_MAX_PERIOD  150000 // Min motor RPM = 1 (before gear)
+#define MOTOR_MIN_PERIOD  150    // Max motor RPM = 1000 (before gear)
+#define MOTOR_ACCEL       1.0    // Acceleration, deceleration
 
 #define MOTOR_DIR_PIN     8
 #define MOTOR_PUL_PIN     9
@@ -14,75 +16,88 @@
 // Test time in seconds
 #define TEST_TIME_SEC     60
 
-
-// Motor methods
-volatile unsigned long step_count = 0; // use volatile for shared variables
-
-// ~882 micro seconds step period
-const double STEP_PERIOD = 60000000. / ((double)MOTOR_STEPS * MOTOR_MICRO_STEPS * MOTOR_GEAR_RATIO * MOTOR_RPM);
-const double MIN_SPEED   = STEP_PERIOD / 1000000.; // max period is 1 second
-const double MAX_SPEED   = STEP_PERIOD / 40.;      // min period is 40 micro seconds
+// Motor step count
+volatile unsigned long motor_step_count = 0; // use volatile for shared variables
 
 static void stepCounterISR()
 {
-    step_count++;
+    motor_step_count++;
 }
+
+static void setMotorStepCount(unsigned long step_count)
+{
+    noInterrupts();
+    motor_step_count = step_count;
+    interrupts();
+}
+
+static unsigned long getMotorStepCount()
+{
+    static unsigned long step_count_copy;
+    noInterrupts();
+    step_count_copy = motor_step_count;
+    interrupts();
+    return step_count_copy;
+}
+
+// Motor
+const double MOTOR_NOM_PERIOD =
+    60000000. / ((double)MOTOR_STEPS * MOTOR_MICRO_STEPS * MOTOR_GEAR_RATIO * MOTOR_RPM); // 882.35 microseconds per step
+double motor_step_period = MOTOR_MAX_PERIOD;
 
 static void setupMotor()
 {
     pinMode(MOTOR_DIR_PIN, OUTPUT);
     digitalWrite(MOTOR_DIR_PIN, MOTOR_DIRECTION);
 
-    Timer1.initialize(1000000); // 1 second period
-    Timer1.pwm(MOTOR_PUL_PIN, MOTOR_PWM_DUTY); // 50% duty
+    motor_step_period = MOTOR_MAX_PERIOD;
+
+    Timer1.initialize((unsigned long)(motor_step_period + 0.5));
+    Timer1.pwm(MOTOR_PUL_PIN, MOTOR_PWM_DUTY);
     Timer1.attachInterrupt(stepCounterISR);
     Timer1.stop();
 }
 
-static void setMotorSpeed(double speed)
+static void setMotorSpeed(double target_period)
 {
-    if (speed > 0.) {
-        if (speed < MIN_SPEED)
-            speed = MIN_SPEED;
-        if (speed > MAX_SPEED)
-            speed = MAX_SPEED;
+    if (target_period < MOTOR_MIN_PERIOD)
+        target_period = MOTOR_MIN_PERIOD;
+    if (target_period > MOTOR_MAX_PERIOD)
+        target_period = MOTOR_MAX_PERIOD;
 
-        Timer1.setPeriod((unsigned long)(STEP_PERIOD / speed + 0.5));
-        Timer1.setPwmDuty(MOTOR_PUL_PIN, MOTOR_PWM_DUTY);
-    }
-    else {
-        Timer1.stop();
-    }
-}
+    double start_speed = MOTOR_NOM_PERIOD / motor_step_period;
+    double end_speed   = MOTOR_NOM_PERIOD / target_period;
+    double speed_diff  = end_speed - start_speed;
 
-static void setMotorStepCount(unsigned long count)
-{
-    noInterrupts();
-    step_count = count;
-    interrupts();
-}
+    long iterations = (long)(fabs(speed_diff) * 1000. / MOTOR_ACCEL + 0.5);
+    if (iterations < 0)
+        iterations = 0;
 
-static unsigned long getMotorStepCount()
-{
-    static unsigned long count_copy;
-    noInterrupts();
-    count_copy = step_count;
-    interrupts();
-    return count_copy;
-}
+    unsigned long start_time, end_time;
+    start_time = micros();
 
-static void delayMicros(unsigned long delay_us, unsigned long start_us = 0)
-{
-    if (delay_us)
+    for (long i = 0; i < iterations; ++i)
     {
-        if (!start_us)
-        {
-            start_us = micros();
-        }
-        // See https://www.gammon.com.au/millis
-        while (micros() - start_us < delay_us)
-            ;
+        motor_step_period = MOTOR_NOM_PERIOD / (start_speed + speed_diff * (double)i / iterations);
+
+        Timer1.setPeriod((unsigned long)(motor_step_period + 0.5));
+        Timer1.setPwmDuty(MOTOR_PUL_PIN, MOTOR_PWM_DUTY);
+
+        end_time = (unsigned long)(i + 1) * 1000; // Every iteration takes 1000 microseconds
+        while (micros() - start_time < end_time)
+            delayMicroseconds(1);
     }
+
+    motor_step_period = target_period;
+
+    Timer1.setPeriod((unsigned long)(motor_step_period + 0.5));
+    Timer1.setPwmDuty(MOTOR_PUL_PIN, MOTOR_PWM_DUTY);
+}
+
+static void stopMotor()
+{
+    setMotorSpeed(MOTOR_MAX_PERIOD);
+    Timer1.stop();
 }
 
 void setup()
@@ -94,47 +109,38 @@ void setup()
 
 void loop()
 {
-    // Delay 5 seconds
+    // Delay 2 seconds
     Serial.println("Loop start.");
     delay(2000);
 
-    // Ramp up motor
-    int iterations = 1000;
-    double ramp_time = 1000000.;
-    Serial.print("Starting with iterations: ");
-    Serial.println(iterations);
-    Serial.print("and ramp time: ");
-    Serial.println(ramp_time / 1000000.);
+    // Accelerate
     unsigned long start_time = micros();
+    stopMotor();
     setMotorStepCount(0);
-    for (int i = 0; i < iterations; ++i) {
-        setMotorSpeed((double)i / iterations);
-        delayMicros((unsigned long)(ramp_time * (i + 1) / iterations) - (micros() - start_time));
-    }
+    setMotorSpeed(MOTOR_NOM_PERIOD);
+    unsigned long ramp_up_steps = getMotorStepCount();
+    unsigned long ramp_up_time  = micros() - start_time;
 
     // Cruise
-    double total_time = TEST_TIME_SEC * 1000000.;
-    double cruise_time = total_time - 2. * ramp_time;
-    double total_steps = total_time / STEP_PERIOD;
-    unsigned long ramp_up_steps = getMotorStepCount();
-    double cruise_steps = total_steps - ramp_up_steps * 2.;
-    double cruise_speed = STEP_PERIOD / (cruise_time / cruise_steps);
-    setMotorSpeed(cruise_speed);
+    double total_time    = TEST_TIME_SEC * 1000000.;
+    double cruise_time   = total_time - ramp_up_time * 2.;
+    double total_steps   = total_time / MOTOR_NOM_PERIOD;
+    double cruise_steps  = total_steps - ramp_up_steps * 2.;
+    double cruise_period = cruise_time / cruise_steps;
+    setMotorSpeed(cruise_period);
     Serial.print("Ramp up steps: ");
     Serial.println(ramp_up_steps);
+    Serial.print("Ramp up time: ");
+    Serial.println(ramp_up_time);
     Serial.print("Cruise speed: ");
-    Serial.println(cruise_speed);
-    unsigned long after_time = (unsigned long)(ramp_time + cruise_time);
-    delayMicros((unsigned long)(ramp_time + cruise_time) - (micros() - start_time));
+    Serial.println(MOTOR_NOM_PERIOD / cruise_period);
+    while (micros() - start_time < (total_time - ramp_up_time))
+        delay(1);
 
-    // Ramp down motor
-    for (int i = 0; i < iterations; ++i) {
-        setMotorSpeed((double)(iterations - i - 1) / iterations);
-        delayMicros((unsigned long)(ramp_time * (i + 1) / iterations) - ((micros() - start_time) - after_time));
-    }
-    setMotorSpeed(0);
+    // Decelerate
+    stopMotor();
     unsigned long final_steps = getMotorStepCount();
-    double final_time = (micros() - start_time);
+    double        final_time  = micros() - start_time;
     Serial.print("Final steps: ");
     Serial.println(final_steps);
     Serial.print("Final time: ");
